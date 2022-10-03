7e523369bb69cda8e1f4a68e56deb7202aad882e
Sandesh Paudel <sandeshpaudel81@gmail.com>
2022-10-03 09:12:42 +0545

    setting up continue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect } from 'react'
+import { collection, getDocs } from 'firebase/firestore'
+import {db} from './firebase-config'
 
 function App() {
+  const usersCollectionRef = collection(db, "users")
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef)
+      console.log(data)
+    }
+    getUsers()
+  }, [])
   return (
     <div className="App">
       <h1>Sandesh Paudel</h1>
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,20 +8,13 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
-//   apiKey: "AIzaSyAy7XDvl-N-MbdDx93Ies6CNTqF81kOvOg",
-//   authDomain: "kadam-myagdi.firebaseapp.com",
-//   projectId: "kadam-myagdi",
-//   storageBucket: "kadam-myagdi.appspot.com",
-//   messagingSenderId: "774905408025",
-//   appId: "1:774905408025:web:d3782e5c44c0f0928c1888",
-//   measurementId: "G-VBXEV06EFL"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
