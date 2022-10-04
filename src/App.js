2da2093273f7188ddc8f3427306e4ffc36c8f4f7
Sandesh Paudel <sandeshpaudel81@gmail.com>
2022-10-04 07:24:02 +0545

    added routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import './App.css';
-import {useEffect } from 'react'
-import { collection, getDocs } from 'firebase/firestore'
-import {db} from './firebase-config'
+// import {useEffect } from 'react'
+// import { collection, getDocs } from 'firebase/firestore'
+// import {db} from './firebase-config'
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import ProjectList from './pages/ProjectList';
 
 function App() {
-  const usersCollectionRef = collection(db, "users")
-  useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(usersCollectionRef)
-      console.log(data)
-    }
-    getUsers()
-  }, [])
+  // const usersCollectionRef = collection(db, "admins")
+  // useEffect(() => {
+  //   const getUsers = async () => {
+  //     const data = await getDocs(usersCollectionRef)
+  //     console.log(data)
+  //   }
+  //   getUsers()
+  // }, [])
   return (
-    <div className="App">
-      <h1>Sandesh Paudel</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} exact/>
+        <Route path="/projects" element={<ProjectList/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/ProjectList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProjectList = () => {
+  return (
+    <div>ProjectList</div>
+  )
+}
+
+export default ProjectList
