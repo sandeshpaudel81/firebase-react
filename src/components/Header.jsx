fb7d1f057e3867886e9a345310828dcaf3d305be
Sandesh Paudel <sandeshpaudel81@gmail.com>
2022-10-11 23:33:26 +0545

    working on navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,75 +3,76 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <nav class="bg-slate-300">
-        <div class="container mx-auto">
-            <div class="flex items-center justify-around font-semibold">
-                <ul class="hidden md:flex uppercase items-center gap-10 text-gray-600">
-                    <li class="hover:text-black"><Link to='/'><p class="p-3">Home</p></Link></li>
-                    <div class="p-3 group cursor-pointer">
-                        <h2 class="group-hover:text-black">About</h2>
-                        <div class="absolute top-10 hidden group-hover:block hover:block duration-700">
-                            <div class="py-3">
-                                <div class="w-4 h-4 left-4 absolute mt-1 bg-slate-300 rotate-45"></div>
+    <nav className="bg-slate-300 shadow-lg shadow-gray-400">
+        <div className="container mx-auto">
+            <div className="flex items-center justify-around font-semibold">
+                <h1 className=''>Logo</h1>
+                <ul className="hidden md:flex uppercase items-center gap-10 text-gray-600">
+                    <li className="hover:text-primary"><Link to='/'><p className="p-3">Home</p></Link></li>
+                    <div className="p-3 group cursor-pointer">
+                        <h2 className="group-hover:text-primary">About</h2>
+                        <div className="absolute top-10 hidden group-hover:block hover:block duration-700">
+                            <div className="py-3">
+                                <div className="w-4 h-4 left-4 absolute mt-1 bg-slate-300 rotate-45"></div>
                             </div>
-                            <div class="bg-slate-300">
-                                <li class="hover:text-black"><Link to='#'><p class="p-3">Objectives</p></Link></li>
-                                <li class="hover:text-black"><Link to='#'><p class="p-3">Vision</p></Link></li>
-                                <li class="hover:text-black"><Link to='#'><p class="p-3">Organogram</p></Link></li>
-                                <li class="hover:text-black"><Link to='#'><p class="p-3">Donors and Partners</p></Link></li>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="p-3 group cursor-pointer">
-                        <h2 class="group-hover:text-black">Projects</h2>
-                        <div class="absolute top-10 hidden group-hover:block hover:block duration-700">
-                            <div class="py-3">
-                                <div class="w-4 h-4 left-4 absolute mt-1 bg-slate-300 rotate-45"></div>
-                            </div>
-                            <div class="bg-slate-300">
-                                <li class="hover:text-black"><Link to='#'><p class="p-3">Ongoing Projects</p></Link></li>
-                                <li class="hover:text-black"><Link to='#'><p class="p-3">Completed Projects</p></Link></li>
-                                <li class="hover:text-black"><Link to='#'><p class="p-3">All Projects</p></Link></li>
+                            <div className="bg-slate-300">
+                                <li className="hover:text-primary"><Link to='#'><p className="p-3">Objectives</p></Link></li>
+                                <li className="hover:text-primary"><Link to='#'><p className="p-3">Vision</p></Link></li>
+                                <li className="hover:text-primary"><Link to='#'><p className="p-3">Organogram</p></Link></li>
+                                <li className="hover:text-primary"><Link to='#'><p className="p-3">Donors and Partners</p></Link></li>
                             </div>
                         </div>
                     </div>
-                    <li class="hover:text-black"><Link to='#'><p class="p-3">News</p></Link></li>
-                    <div class="p-3 group cursor-pointer">
-                        <h2 class="group-hover:text-black">Team</h2>
-                        <div class="absolute top-10 hidden group-hover:block hover:block duration-700">
-                            <div class="py-3">
-                                <div class="w-4 h-4 left-4 absolute mt-1 bg-slate-300 rotate-45"></div>
+                    <div className="p-3 group cursor-pointer">
+                        <h2 className="group-hover:text-primary">Projects</h2>
+                        <div className="absolute top-10 hidden group-hover:block hover:block duration-700">
+                            <div className="py-3">
+                                <div className="w-4 h-4 left-4 absolute mt-1 bg-slate-300 rotate-45"></div>
                             </div>
-                            <div class="bg-slate-300">
-                                <li class="hover:text-black"><Link to='#'><p class="p-3">Board Members</p></Link></li>
-                                <li class="hover:text-black"><Link to='#'><p class="p-3">Advisors</p></Link></li>
-                                <li class="hover:text-black"><Link to='#'><p class="p-3">General Members</p></Link></li>
-                                <li class="hover:text-black"><Link to='#'><p class="p-3">Life Members</p></Link></li>
+                            <div className="bg-slate-300">
+                                <li className="hover:text-primary"><Link to='#'><p className="p-3">Ongoing Projects</p></Link></li>
+                                <li className="hover:text-primary"><Link to='#'><p className="p-3">Completed Projects</p></Link></li>
+                                <li className="hover:text-primary"><Link to='#'><p className="p-3">All Projects</p></Link></li>
                             </div>
                         </div>
                     </div>
-                    <li class="hover:text-black"><Link to='#'><p class="p-3">Get Involved</p></Link></li>
-                    <li class="hover:text-black"><Link to='#'><p class="p-3">Publications</p></Link></li>
-                    <li class="hover:text-black"><Link to='#'><p class="p-3">Contact</p></Link></li>
+                    <li className="hover:text-primary"><Link to='#'><p className="p-3">News</p></Link></li>
+                    <div className="p-3 group cursor-pointer">
+                        <h2 className="group-hover:text-primary">Team</h2>
+                        <div className="absolute top-10 hidden group-hover:block hover:block duration-700">
+                            <div className="py-3">
+                                <div className="w-4 h-4 left-4 absolute mt-1 bg-slate-300 rotate-45"></div>
+                            </div>
+                            <div className="bg-slate-300">
+                                <li className="hover:text-primary"><Link to='#'><p className="p-3">Board Members</p></Link></li>
+                                <li className="hover:text-primary"><Link to='#'><p className="p-3">Advisors</p></Link></li>
+                                <li className="hover:text-primary"><Link to='#'><p className="p-3">General Members</p></Link></li>
+                                <li className="hover:text-primary"><Link to='#'><p className="p-3">Life Members</p></Link></li>
+                            </div>
+                        </div>
+                    </div>
+                    <li className="hover:text-primary"><Link to='#'><p className="p-3">Get Involved</p></Link></li>
+                    <li className="hover:text-primary"><Link to='#'><p className="p-3">Publications</p></Link></li>
+                    <li className="hover:text-primary"><Link to='#'><p className="p-3">Contact</p></Link></li>
                 </ul>
-                <ul class="sidebar md:hidden bg-primaryRegular absolute w-full h-full bottom-0 text-white duration-500 left-[-100%]">
-                    <div class="flex">
+                <ul className="sidebar md:hidden bg-primaryRegular absolute w-full h-full bottom-0 text-white duration-500 left-[-100%]">
+                    <div className="flex">
                         <img src="#" alt="logo"/>
-                        <div class="absolute right-4 cursor-pointer" onclick="HideSidebar()"><i class="fa-solid fa-xmark text-2xl text-primaryExtraDark"></i></div>
+                        <div className="absolute right-4 cursor-pointer" onclick="HideSidebar()"><i className="fa-solid fa-xmark text-2xl text-primaryExtraDark"></i></div>
                     </div>
-                    <li class="p-3"><Link to='#'>Home</Link></li>
-                    <li class="p-3"><Link to='#'>About</Link></li>
-                    <li class="p-3"><Link to='#'>News/Events</Link></li>
-                    <li class="p-3"><Link to='#'>Team</Link></li>
-                    <li class="p-3"><Link to='#'>Articles</Link></li>
-                    <li class="p-3"><Link to='#'>Downloads</Link></li>
-                    <li class="p-3"><Link to='#'>Contact</Link></li>
-                    <li class="p-3 my-1 rounded-lg bg-secondaryR"><Link to='#'>Jaycess Cup 2022</Link></li>
+                    <li className="p-3"><Link to='#'>Home</Link></li>
+                    <li className="p-3"><Link to='#'>About</Link></li>
+                    <li className="p-3"><Link to='#'>News/Events</Link></li>
+                    <li className="p-3"><Link to='#'>Team</Link></li>
+                    <li className="p-3"><Link to='#'>Articles</Link></li>
+                    <li className="p-3"><Link to='#'>Downloads</Link></li>
+                    <li className="p-3"><Link to='#'>Contact</Link></li>
+                    <li className="p-3 my-1 rounded-lg bg-secondaryR"><Link to='#'>Jaycess Cup 2022</Link></li>
                 </ul>
             </div>
-            <div class="md:hidden flex p-3">
-                <div class="text-white">Current Page</div>
-                <div class="text-white ml-auto cursor-pointer" onclick="OpenSidebar()"><i class="fa-solid fa-bars"></i></div>
+            <div className="md:hidden flex p-3">
+                <div className="text-white">Current Page</div>
+                <div className="text-white ml-auto cursor-pointer" onclick="OpenSidebar()"><i className="fa-solid fa-bars"></i></div>
             </div>
         </div>
     </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         xl: "1280px",
         "2xl": "1536px",
       },
+      colors: {
+        primary: "#0080ff"
+      }
     },
   },
   plugins: [],
