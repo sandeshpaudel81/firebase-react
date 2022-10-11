7c110ff95490f19f36c0dad0b6851a63d5314af2
Sandesh Paudel <sandeshpaudel81@gmail.com>
2022-10-11 23:08:51 +0545

    tailwind conf extend

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = () => {
-  return (
-    <div>Let's work on header</div>
-  )
-}
-
-export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
   },
   plugins: [],
 }
