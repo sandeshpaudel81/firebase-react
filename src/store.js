84e07c64be451d554b2da04c884added1d2da2f3
Sandesh Paudel <sandeshpaudel81@gmail.com>
2022-10-17 14:58:34 +0545

    project reducer registration

--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import projectReducer from './slices/projectSlice'
 
 export const store = configureStore({
     reducer: {
-        
+        project: projectReducer,
     },
 })
