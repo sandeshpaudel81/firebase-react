d1c68930b093884e7a5d69bafcb98d095fa40fed
Sandesh Paudel <sandeshpaudel81@gmail.com>
2022-10-28 15:32:58 +0545

    working on home: message from chairman

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,15 @@ const Home = () => {
         <div>
             {/* Carousel */}
             <Carousel />
+
+            {/* Message from Chairman */}
+            <div className='container mx-auto py-10 bg-slate-300'>
+                <div className='border-l-8 border-primary px-5'>
+                    <h2 className='text-primary font-bold text-3xl'>Message From <span className='text-primaryDark'>Chairman</span></h2>
+                    <p className='uppercase text-gray-600 text-sm font-medium'>Welcome message | What is Kadam? | What We Do</p>
+                </div>
+            </div>
+
             <div>Projects</div>
             <div>News</div>
             <div>Awards & Achievements</div>
