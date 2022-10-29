0853659ba07fccdb4c2703d533110faa8c12fba5
Sandesh Paudel <sandeshpaudel81@gmail.com>
2022-10-29 08:59:48 +0545

    home:projects done

---
+++ src/components/Project.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Project = (props) => {
+  return (
+    <div className='col-span-2 md:col-span-1'>
+        <Link to="#">
+        <div className='grid grid-cols-5 bg-slate-300 shadow-sm shadow-white hover:shadow-md hover:shadow-white hover:scale-[1.02] duration-300 rounded-lg overflow-hidden group'>
+            <div className='col-span-5 md:col-span-2'>
+                <img src={props.image} alt={props.content} className="w-full h-[250px] md:h-[200px] object-cover object-center"/>
+            </div>
+            <div className='col-span-5 md:col-span-3 relative p-5'>
+                <div className='w-5 h-5 rotate-45 bg-slate-300 absolute left-1/2 -top-2 md:top-1/2 md:-left-2'></div>
+                <h2 className='font-bold text-xl group-hover:text-primaryD'>{props.title}</h2>
+                <p>{props.content}</p>
+                <button className='bg-primary text-white px-5 py-2 rounded-lg mt-5 group-hover:bg-primaryDark'>See More...</button>
+            </div>
+        </div>
+        </Link>
+    </div>
+  )
+}
+
+export default Project
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,15 @@ import Carousel from '../components/Carousel'
 import Chairman from '../assets/chairman-image.png'
 import projectImage from '../assets/car1.jpg'
 import { Link } from 'react-router-dom'
+import Project from '../components/Project'
+import { FaChevronRight } from 'react-icons/fa'
 
 const Home = () => {
     const projects = [
-        {"image": projectImage, "title": "Title 1", "content": "content2"},
-        {"image": projectImage, "title": "Title 2", "content": "content1"},
-        {"image": projectImage, "title": "Title 3", "content": "content3"},
-        {"image": projectImage, "title": "Title 4", "content": "content4"}
+        {"image": projectImage, "title": "Title 10", "content": "content2"},
+        {"image": projectImage, "title": "Title 20", "content": "content1"},
+        {"image": projectImage, "title": "Title 30", "content": "content3"},
+        {"image": projectImage, "title": "Title 40", "content": "content4"}
     ]
     return (
         <div>
@@ -42,32 +44,25 @@ const Home = () => {
 
             {/* Projects */}
             <div className='projectsBg bg-slate-900'>
-                <div className='container mx-auto px-5 py-20'>
+                <div className='container mx-auto px-5 py-10 md:py-20'>
                     <div className='border-l-8 border-primary px-5'>
                         <h2 className='text-primary font-bold text-3xl'>Our <span className='text-white'>Projects</span></h2>
                         <p className='uppercase text-gray-400 text-sm font-medium mt-2'>since 2001</p>
                     </div>
                     <div className='grid grid-cols-2 gap-10 mt-5 md:mt-10'>
                         {projects.map((project) => (
-                            <div className='col-span-2 md:col-span-1'>
-                                <Link to="#">
-                                <div className='grid grid-cols-5 bg-slate-300 shadow-md shadow-white hover:shadow-lg hover:shadow-white hover:scale-[1.02] duration-300 rounded-lg overflow-hidden group'>
-                                    <div className='col-span-5 md:col-span-2'>
-                                        <img src={project.image} alt={project.content} className="w-full h-[250px] md:h-[200px] object-cover object-center"/>
-                                    </div>
-                                    <div className='col-span-5 md:col-span-3 relative p-5'>
-                                        <div className='w-5 h-5 rotate-45 bg-slate-300 absolute left-1/2 -top-2 md:top-1/2 md:-left-2'></div>
-                                        <h2 className='font-bold text-xl group-hover:text-primaryD'>{project.title}</h2>
-                                        <p>{project.content}</p>
-                                        <button className='bg-primary text-white px-5 py-2 rounded-lg mt-5 group-hover:bg-primaryDark'>See More...</button>
-                                    </div>
-                                </div>
-                                </Link>
-                            </div>
+                            <Project content={project.content} image={project.image} title={project.title}/>
                         ))}
+                    </div>
+                    <div className='flex justify-end mt-5'>
+                        <Link to="/projects" className='uppercase text-gray-400 hover:text-white'>
+                            <p className='flex items-center font-medium'>See More <FaChevronRight className='ml-3'/></p>
+                        </Link>
                     </div>
                 </div>
             </div>
+
+            {/* Projects */}
             <div>News</div>
             <div>Awards & Achievements</div>
             <div>Gallery</div>
