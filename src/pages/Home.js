25bf47dcff1f2e1f6d3a0eaea864f380c10b7a0f
Sandesh Paudel <sandeshpaudel81@gmail.com>
2022-10-30 22:06:22 +0545

    home: news date

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -72,16 +72,16 @@ const Home = () => {
                     <div className='grid grid-cols-4 gap-10 mt-5 md:mt-10'>
                         {projects.map((project) => (
                             <div className='col-span-4 md:col-span-1 shadow-lg h-80 relative group overflow-hidden'>
-                                <div className='absolute bg-primary rounded-full right-0 text-white'>
-                                    <div className='flex'>
-                                        <h3 className='text-3xl'>30</h3>
-                                        <p className='flex flex-col'>Oct <span>2022</span></p>
+                                <div className='absolute bg-primary rounded-full right-2 top-2 z-30 text-white'>
+                                    <div className='flex flex-col items-center font-semibold w-14 h-14'>
+                                        <h3 className='text-2xl'>30</h3>
+                                        <p className='text-sm uppercase'>Oct</p>
                                     </div>
                                 </div>
-                                <div className='overflow-hidden'>
+                                <div className='overflow-hidden z-10'>
                                     <img src={project.image} alt={project.title} className='w-full h-[250px] object-cover object-center group-hover:scale-[1.1] group-hover:blur-[1px] duration-300'/>
                                 </div>
-                                <div className='bg-white absolute p-5 bottom-[-120px] group-hover:bottom-0 duration-500'>
+                                <div className='bg-white absolute p-5 bottom-[-120px] group-hover:bottom-0 duration-500 z-20'>
                                     <h2 className='text-xl font-bold'>{project.title}</h2>
                                     <p className='text-gray-500 h-24 opacity-0 overflow-hidden group-hover:opacity-100'>{project.content}</p>
                                     <small className='flex items-center mt-2 font-medium text-gray-600 opacity-0 group-hover:opacity-100'><FaRegHourglass className='mr-2'/>5 min read</small>
